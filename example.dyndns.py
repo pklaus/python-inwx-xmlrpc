df47e80d0eea9c2e2c50732473236f390934ec74
Markus Roth <mail@rothmark.us>
2014-03-08 13:44:13 +0100

    Set both IPv4 and IPv6, depending on type (A, AAAA).
    Cave: default_ip is not used anymore.

--- example.dyndns.py
+++ example.dyndns.py
@@ -2,6 +2,7 @@
 # -*- encoding: UTF8 -*-
 
 # author: Philipp Klaus, philipp.klaus →AT→ gmail.com
+# author: Markus Roth,   mail →AT→ rothmark.us
 
 # This file is part of python-inwx-xmlrpc.
 #
@@ -26,31 +27,42 @@ from inwx import domrobot
 from configuration import get_account_data, get_domain_update
 from urllib import urlopen
 
-IPV6_DETECTION_API = 'http://v6.ipv6-test.com/api/myip.php'
+# Mapping from type to webservice for external IP determination
+record_type_to_api = {
+    'A':    'http://v4.ipv6-test.com/api/myip.php',
+    'AAAA': 'http://v6.ipv6-test.com/api/myip.php'
+}
+
+# Call webservice to obtain external IP
+def get_ip(api):
+    try:
+        ip = urlopen(api).read().decode('ascii')
+        return ip
+    except:
+        return None
 
 def main():
     api_url, username, password = get_account_data(True)
-    domain, subdomain, default_ip = get_domain_update(True)
-    try:
-        new_ip = urlopen(IPV6_DETECTION_API).read().decode('ascii')
-    except:
-        # If something failed with the IPv6 detection, we may abort at this point
-        return
-        # or simply set the default value:
-        new_ip = default_ip
+    domain, subdomain, _ = get_domain_update(True)
+
     # Instantiate the inwx class (does not connect yet but dispatches calls to domrobot objects with the correct API URL
     inwx_conn = domrobot(api_url, username, password, 'en', False)
-    # get all the nameserver entries for a certain domain 
     nsentries = inwx_conn.nameserver.info({'domain': domain})
-    for record in nsentries['record']:
-        if subdomain == record['name']:
-            id = record['id']
-            break
-    if id:
-        print "Setting subdomain %s to the new IPv6 IP %s." % (subdomain, new_ip)
-        inwx_conn.nameserver.updateRecord({'id':id,'content':new_ip,'ttl':3600})
-    else:
-        print "Subdomain not in list of nameserver entries."
 
+    # Filter entries for subdomain
+    nsentries = [record for record in nsentries['record'] if subdomain == record['name']]
+
+    assert nsentries, "Subdomain %s not in list of nameserver entries." % subdomain
+
+    # There may be multiple entries for one subdomain, one for each type (A, AAAA, ...)
+    for record in nsentries:
+        record_type = record['type'] # A, AAAA
+        assert record_type in record_type_to_api, "Unsupported record type: %s" % record_type
+        
+        new_ip = get_ip(record_type_to_api[record_type])
+        if new_ip:
+            print "Updating %s record of %s from %s to %s" % (record_type, record['name'], record['content'], new_ip)
+            inwx_conn.nameserver.updateRecord({'id': record['id'], 'content': new_ip, 'ttl': 3600})
+            
 if __name__ == '__main__':
     main()
